e6ade2e5ae64f53b67b9cd6e23b5f59182589e41
Accurics <admin@accurics.com>
2021-03-18 19:10:48 -0500

    Auto Remediation By Accurics

---
+++ vpc.tf
@@ -0,0 +1,55 @@
+
+resource "aws_flow_log" "aws-demo" {
+  vpc_id          = "${aws_vpc.aws-demo.id}"
+  iam_role_arn    = "##arn:aws:iam::111111111111:role/sample_role##"
+  log_destination = "${aws_s3_bucket.aws-demo.arn}"
+  traffic_type    = "ALL"
+
+  tags = {
+    GeneratedBy      = "Accurics"
+    ParentResourceId = "aws_vpc.aws-demo"
+  }
+}
+resource "aws_s3_bucket" "aws-demo" {
+  bucket        = "aws-demo_flow_log_s3_bucket"
+  acl           = "private"
+  force_destroy = true
+
+  versioning {
+    enabled    = true
+    mfa_delete = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+resource "aws_s3_bucket_policy" "aws-demo" {
+  bucket = "${aws_s3_bucket.aws-demo.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "aws-demo-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.aws-demo.id}/*"
+    }
+  ]
+}
+POLICY
+}
